2ec2fe2a6437bd3af4f0998d714cde8eea727217
Tanzim52 <mahin1575@gmail.com>
2025-01-27 20:19:03 +0600

    added banner slider

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
+          <div className='pb-3 mb-4'>
+          <Banner></Banner>
+          </div>
           This is home.  
         </div>
     );
